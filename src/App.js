ca482860f35022917959b8060e4263d6574184fb
SujalChavan <160272159+Chavansujal@users.noreply.github.com>
2025-09-06 00:32:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import axios from "axios";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://ourtalks-backend.onrender.com");
 
 function App() {
   const [user, setUser] = useState(null); // Logged-in user
